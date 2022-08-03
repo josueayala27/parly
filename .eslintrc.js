a0c54a17f748ff46b13e64a30a9e3fc659ec27d1
Josué Ayala <ayalavalencia11@gmail.com>
2022-08-03 11:54:50 -0600

    Nuxt installation

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+  },
+};
