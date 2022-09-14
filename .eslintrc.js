d0cebac4c9e5ba6b4a80dd72bb84c5b4c591be68
Josué Ayala <ayalavalencia11@gmail.com>
2022-09-14 13:34:22 -0600

    refactor: 💡 Update code linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
-module.exports = {
+export default {
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: ['next', 'airbnb', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "prettier"],
+  plugins: ['react', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "no-unused-vars": "warn",
-    "no-console": "off",
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
 };
