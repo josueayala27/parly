035dabe646c05a16aeb7e369d98d5a70b8d28f3e
Josué Ayala <ayalavalencia11@gmail.com>
2022-09-17 10:52:00 -0600

    style: 💄 Update code style

--- components/meet/participant/Participant.jsx
+++ components/meet/participant/Participant.jsx
@@ -1,12 +1,12 @@
 import useAxios from '@/hooks/useAxios';
 import { setParticipants } from '@/store/slices/participantSlice';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ParticipantItem from './ParticipantItem';
 
 export default function Participant() {
-  const { data, loading, error } = useAxios({ method: 'get', url: '/users' });
-  const participant = useSelector((state) => state.participant);
+  const { data, loading } = useAxios({ method: 'get', url: '/users' });
+  const { participants } = useSelector((state) => state.participant);
 
   const dispatch = useDispatch();
 
@@ -17,11 +17,11 @@ export default function Participant() {
   return (
     <div className="w-full flex-1 overflow-hidden flex-col flex gap-3">
       <h4>Participants</h4>
-      {loading && !participant.participants ? (
+      {loading && !participants ? (
         <div>Loading...</div>
       ) : (
         <div className="flex-1 overflow-auto flex flex-col gap-3">
-          {participant.participants.map((participant) => (
+          {participants.map((participant) => (
             <ParticipantItem {...participant} key={participant.id} />
           ))}
         </div>
