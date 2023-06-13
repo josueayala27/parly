27195e477773f5c3c14e5890ff19e5dddcdbad0d
Josué Ayala <ayalavalencia11@gmail.com>
2023-06-13 14:09:58 -0600

    tailwind installation

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex items-center justify-center h-screen bg-black text-white">
+    Hello from index.vue
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
