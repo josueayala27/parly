2118bcde4405e06591b444d6d6c1ffbee82ad137
Josué Ayala <ayalavalencia11@gmail.com>
2023-06-29 15:19:08 -0600

    Update chat information section

---
+++ components/chat/information/Action.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="flex justify-between items-center p-3 hover:bg-gray-100 cursor-pointer rounded-lg"
+  >
+    <div class="flex gap-4">
+      <icon size="20px" :name="icon" />
+      <UiCaption size="sm">
+        {{ title }}
+      </UiCaption>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ title: string; icon: String }>();
+</script>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,5 @@ export default <Partial<Config>>{
   theme: {
     extend: {},
   },
+  plugins: [require("tailwind-scrollbar-hide")],
 };
